8417e9540c1acbd2f70f6e543d70efc234cc8a93
Pedro Carara <cararax@gmail.com>
2023-01-28 12:12:57 -0300

    feat: add vote entity model and some validations

--- src/main/java/com/carara/vote/domain/entity/Vote.java
+++ src/main/java/com/carara/vote/domain/entity/Vote.java
@@ -1,29 +1,51 @@
 package com.carara.vote.domain.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
+import org.hibernate.Hibernate;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class Vote {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @Positive
     @Column(name = "associateId", nullable = false)
     private Long associateId;
 
+    @Positive
     @Column(name = "agendaId", nullable = false)
     private Long agendaId;
 
+    @NotNull
     @Column(name = "voteOption", nullable = false)
     private VoteOption voteOption;
 
+    @NotNull
     @Column(name = "voteTime", nullable = false)
     private LocalDateTime voteTime;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Vote vote = (Vote) o;
+        return id != null && Objects.equals(id, vote.id);
+    }
 
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
