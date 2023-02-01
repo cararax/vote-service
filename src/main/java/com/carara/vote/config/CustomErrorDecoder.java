efa79511d2e6462f51aaa9a3fcef806036345ca2
Pedro Carara <cararax@gmail.com>
2023-02-01 12:10:11 -0300

    feat: add error decoder on feign requests to proper handle exceptions

--- src/main/java/com/carara/vote/config/CustomErrorDecoder.java
+++ src/main/java/com/carara/vote/config/CustomErrorDecoder.java
@@ -3,7 +3,9 @@ package com.carara.vote.config;
 import feign.Response;
 import feign.codec.ErrorDecoder;
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class CustomErrorDecoder implements ErrorDecoder {
     @Override
     public Exception decode(String s, Response response) {
