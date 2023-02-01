076eb4e634562e1106c049c8c539c182fd50cd8b
Pedro Carara <cararax@gmail.com>
2023-02-01 15:10:12 -0300

    refactor: separated business roles into methods on voteservice

--- src/main/java/com/carara/vote/service/VoteService.java
+++ src/main/java/com/carara/vote/service/VoteService.java
@@ -10,6 +10,7 @@ import com.carara.vote.infra.client.AssociateClient;
 import com.carara.vote.infra.client.response.AgendaResponse;
 import com.carara.vote.infra.repository.VoteRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,7 @@ import java.util.Objects;
 
 @Service
 @AllArgsConstructor
+@Log4j2(topic = "VoteService")
 public class VoteService {
 
     VoteRepository voteRepository;
@@ -27,25 +29,41 @@ public class VoteService {
     AgendaClient agendaClient;
 
     public Vote createVote(VoteRequest voteRequest) throws VotingTimeExpiredException, AssociateAlreadyVotedException {
-//        boolean existsByAssociateId = voteRepository.existsByAssociateId(voteRequest.getAssociateId());
-//        if (existsByAssociateId) {
-//            throw new AssociateAlreadyVotedException();
-//        }
-
-        ResponseEntity<AgendaResponse> agendaResponse = agendaClient.checkIfAgendaExists(voteRequest.getAgendaId());
-//        if (Objects.requireNonNull(agendaResponse.getBody()).endDate().isBefore(LocalDateTime.now())) {
-//            throw new VotingTimeExpiredException();
-//        }
-
-        associateClient.checkIfAssociateExists(voteRequest.getAssociateId());
+        log.info("Validatig vote for agenda id: " + voteRequest.getAgendaId() + " and associate id: "
+                + voteRequest.getAssociateId());
+        validateVote(voteRequest);
+        log.info("Vote request sucessfully validated");
 
         Vote voteEntity = new Vote();
         BeanUtils.copyProperties(voteRequest, voteEntity);
         voteEntity.setVoteTime(LocalDateTime.now());
-
-        //todo: send request for email confirmation
-
         return voteRepository.save(voteEntity);
+    }
+
+    private void validateVote(VoteRequest voteRequest) {
+        checkIfUserAlreadyVoted(voteRequest);
+        checkIfAgendaExistAndIsNotExpired(voteRequest);
+        checkIfAssociateExists(voteRequest);
+    }
+
+    private void checkIfUserAlreadyVoted(VoteRequest voteRequest) {
+        boolean existsByAssociateId = voteRepository.existsByAssociateId(voteRequest.getAssociateId());
+        if (existsByAssociateId) {
+            log.error("Associate id: " + voteRequest.getAssociateId() + " already voted");
+            throw new AssociateAlreadyVotedException();
+        }
+    }
+
+    private void checkIfAgendaExistAndIsNotExpired(VoteRequest voteRequest) {
+        ResponseEntity<AgendaResponse> agendaResponse = agendaClient.checkIfAgendaExists(voteRequest.getAgendaId());
+        if (Objects.requireNonNull(agendaResponse.getBody()).endDate().isBefore(LocalDateTime.now())) {
+            log.error("Agenda id: " + voteRequest.getAgendaId() + " expired at " + agendaResponse.getBody().endDate());
+            throw new VotingTimeExpiredException();
+        }
+    }
+
+    private void checkIfAssociateExists(VoteRequest voteRequest) {
+        associateClient.checkIfAssociateExists(voteRequest.getAssociateId());
     }
 
     public List<Vote> getVotesByAgendaId(Long agendaId) {
